a30ee2823d479b69fdc5c798f93f3870d556f62a
mobin-zaman <mobinzamandev@gmail.com>
2020-10-05 12:19:34 +0600

    seat category model added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ShipSessionModule } from './ship-session/ship-session.module';
-import { Ship } from './ship-session/ship.entity';
-import { Routes } from './ship-session/routes.entity';
+import { ShipSessionModule } from './shipdb/ship-session.module';
+import { Ship } from './shipdb/ship.entity';
+import { Routes } from './shipdb/routes.entity';
 
 @Module({
   imports: [
