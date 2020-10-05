6e0bfa9204c18a35557dc4cf1fd67095fcde0b7f
mobin-zaman <mobinzamandev@gmail.com>
2020-10-05 12:19:34 +0600

    seat category model added

---
+++ src/shipdb/seat-category.entity.ts
@@ -0,0 +1,20 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+/**
+ * This is also called class of a ship
+ * class - category of the ship
+ * deck - the floor of the ship
+ */
+export class SeatCategory extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  categoryName: string;
+
+  @Column()
+  categoryButtonSelector: string;
+
+}
