1b7d8e8af00540be0740faf403e2c2e66497be7b
mobin-zaman <mobinzamandev@gmail.com>
2020-10-07 22:56:39 +0600

    ShipIdValidationPipe work is done.

--- src/booking/pipes/seatid-categoryid-validation.pipe.ts
+++ src/booking/pipes/seatid-categoryid-validation.pipe.ts
@@ -16,14 +16,31 @@ export class ShipIdCategoryIdValidationPipe implements PipeTransform {
   constructor(private shipService: ShipSessionService) {}
 
 
+
   async transform(value: GetSeatCategoryInfoDto, metadata: ArgumentMetadata): Promise<any> {
     console.log('getting the value: ', value);
 
+    /**
+     * This is a callback function to check categoruId validity
+     * works with seatCategories.some()
+     * @param obj
+     */
+    const checkCategoryId = obj => {
+      return obj.id === value.categoryId;
+    }
+
     try {
       const seatCategories: SeatCategory[] =await this.shipService.getSeatCategories(value.shipId);
-      console.log(seatCategories);
+
+      console.log(seatCategories.some(checkCategoryId))
+      if(!seatCategories.some(checkCategoryId)){
+        console.log("matched id: ",value.categoryId) ;
+        throw new BadRequestException(`There is no seat category for ${value.categoryId} on this ship`);
+      }
     } catch(e) {
-      throw new BadRequestException(`There is no ship with id: ${value.shipId}`)
+      // console.log("Error validating seat categories: ",e);
+      if(e.message === "Ship not found") throw new BadRequestException(`There is no ship with id: ${value.shipId}`)
+      else throw e;
     }
 
     return value;
