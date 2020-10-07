d88f5860f48e316d61ddc545485be5b280671f71
mobin-zaman <mobinzamandev@gmail.com>
2020-10-07 11:45:53 +0600

    backup database sql file added

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 import { ShipSessionModule } from '../ship-session/ship-session.module';
-import { ShipSessionService } from '../ship-session/ship-session.service';
 
 @Module({
   imports:[ShipSessionModule],
