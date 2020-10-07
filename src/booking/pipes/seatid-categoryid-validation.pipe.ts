b4e87f7b75fcc0f315a950ec463b44ab00ccdb14
mobin-zaman <mobinzamandev@gmail.com>
2020-10-07 21:19:08 +0600

    shipId validatoin for the controller get seat category info is added

--- src/booking/pipes/seatid-categoryid-validation.pipe.ts
+++ src/booking/pipes/seatid-categoryid-validation.pipe.ts
@@ -6,11 +6,26 @@ import {
   Inject,
   Injectable,
 } from '@nestjs/common';
+import { ShipSessionService } from '../../ship-session/ship-session.service';
+import { SeatCategory } from '../../ship-session/seat-category.entity';
+import { GetSeatCategoryInfoDto } from '../dto/get-seat-category-info.dto';
 
 @Injectable()
-export class SeatidCategoryidValidationPipe implements PipeTransform {
-  transform(value: number, metadata: ArgumentMetadata): any {
+export class ShipIdCategoryIdValidationPipe implements PipeTransform {
+
+  constructor(private shipService: ShipSessionService) {}
+
+
+  async transform(value: GetSeatCategoryInfoDto, metadata: ArgumentMetadata): Promise<any> {
     console.log('getting the value: ', value);
+
+    try {
+      const seatCategories: SeatCategory[] =await this.shipService.getSeatCategories(value.shipId);
+      console.log(seatCategories);
+    } catch(e) {
+      throw new BadRequestException(`There is no ship with id: ${value.shipId}`)
+    }
+
     return value;
   }
 }
