f2e745aaab17420d17c74e0cb1f1ca45f818e36a
mobin-zaman <mobinzamandev@gmail.com>
2020-10-07 10:34:06 +0600

    booking module works initiated

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { BookingController } from './booking.controller';
+import { BookingService } from './booking.service';
+import { ShipSessionModule } from '../ship-session/ship-session.module';
+import { ShipSessionService } from '../ship-session/ship-session.service';
 
-@Module({})
+@Module({
+  imports:[ShipSessionModule],
+  controllers: [BookingController],
+  providers: [BookingService, ]
+})
 export class BookingModule {}
