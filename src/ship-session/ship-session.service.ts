2f9016aff1acc6cef08af4e5c0fe95e78904f847
mobin-zaman <mobinzamandev@gmail.com>
2020-10-08 11:50:35 +0600

    login function in the scrapper is done, also the screenshot funciton is working fine

--- src/ship-session/ship-session.service.ts
+++ src/ship-session/ship-session.service.ts
@@ -4,14 +4,15 @@ import { Ship } from './ship.entity';
 import { Repository } from 'typeorm';
 import { GetSeatCategoryInfoDto } from '../booking/dto/get-seat-category-info.dto';
 import { SeatCategory } from './seat-category.entity';
-import { ScrappingService} from './ship-srapper.service';
+import { ScrappingService } from './ship-srapper';
 
 @Injectable()
 export class ShipSessionService {
   constructor(
     @InjectRepository(Ship)
     private shipRepository: Repository<Ship>,
-    @InjectRepository(SeatCategory)private seatCategoryRepository: Repository<SeatCategory>,
+    @InjectRepository(SeatCategory)
+    private seatCategoryRepository: Repository<SeatCategory>,
   ) {}
 
   getAllShip(): Promise<Ship[]> {
@@ -29,19 +30,13 @@ export class ShipSessionService {
   async getSeatCategoryInformation(
     getSeatCategoryInfoDto: GetSeatCategoryInfoDto,
   ) {
-    // console.log('This is the seat catefory infolr', getSeatCategoryInfoDto);
-    const {categoryId} = await getSeatCategoryInfoDto;
+    const { categoryId } = await getSeatCategoryInfoDto;
 
     const category = await this.seatCategoryRepository.findOne(categoryId);
+    const ship = await category.ship;
+    console.log('this is the category: ', category);
 
     const scrapper = await ScrappingService.build();
-
-    console.log("scrapper: ", scrapper);
-
-
-
-
-
-
+    await scrapper.login(ship.shipAdminPageUrl, ship.username, ship.password);
   }
 }
