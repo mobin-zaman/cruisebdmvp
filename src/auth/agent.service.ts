7c26a3ebd6d660a2480c522ad4391e83add9e821
mobin-zaman <mobinzamandev@gmail.com>
2020-11-27 11:56:21 +0600

    agent guard and agent services added

---
+++ src/auth/agent.service.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable, } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FirebaseService } from './firebase.service';
+import { Repository } from 'typeorm';
+import { Agent } from './agent.entity';
+
+@Injectable()
+export class AgentService {
+  constructor(
+    @InjectRepository(AgentService) private agentRepository: Repository<Agent>,
+    @Inject(FirebaseService) private firebaseService: FirebaseService,
+  ) {}
+
+
+  async getAgentByFirebaseIdToken(firebaseIdToken: string): Promise<Agent> {
+    try{
+      const uid = await this.firebaseService.getUidFromFirebaseIdToken(firebaseIdToken);
+      console.log('uid: ', uid);
+      //
+      return await this.agentRepository.findOne({
+        firebase_uid: uid
+      });
+
+    } catch(e){
+      console.log("ERROR: agentService.getAgentByFirebaseIdToken: ", e.message);
+      throw e;
+    }
+  }
+}
