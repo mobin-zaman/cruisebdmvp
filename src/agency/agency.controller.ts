101b77276b422cd179c637d2d82588c544153e3b
mobin-zaman <mobinzamandev@gmail.com>
2020-11-27 13:06:58 +0600

    get all agencies added

--- src/agency/agency.controller.ts
+++ src/agency/agency.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AdminGuard } from 'src/auth/admin.guard';
 import { AgencyService } from './agency.service';
 import { AddAgencyDto } from './dto/addAgency.dto';
@@ -17,4 +17,11 @@ export class AgencyController {
     addAgency(@Body() addAgencyDto: AddAgencyDto) {
         return this.agencyService.addAgency(addAgencyDto);
     }
+
+    @UseGuards(AdminGuard) 
+    @Get('/')
+    getAllAgency() {
+        return this.agencyService.getAllAgency();
+    }
+    
 }
