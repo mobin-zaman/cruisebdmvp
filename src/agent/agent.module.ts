e8f24416849452d73e91b39e0b308048ce666cdb
mobin-zaman <mobinzamandev@gmail.com>
2020-11-27 14:02:24 +0600

    agent module added, and also the realtion between agent and agency has been established through orm

--- src/agent/agent.module.ts
+++ src/agent/agent.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AgentService } from './agent.service';
 import { AgentController } from './agent.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Agent } from '../auth/agent.entity';
 
 @Module({
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([Agent])
+],
   providers: [AgentService],
   controllers: [AgentController]
 })
--- src/agent/agent.service.ts
+++ src/agent/agent.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Agent } from 'http';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AgentService {}
+export class AgentService {
+    constructor(
+        @InjectRepository(Agent) private agentRepository:Repository<Agent>
+    ) {}
+
+
+
+}
