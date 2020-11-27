05c0823069a641d03c23313c0f9b91aa575df154
mobin-zaman <mobinzamandev@gmail.com>
2020-11-27 14:17:44 +0600

    agent work is going on

--- src/agent/agent.service.ts
+++ src/agent/agent.service.ts
@@ -2,10 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Agent } from 'http';
 import { Repository } from 'typeorm';
+import { CreateAgentDto } from './dto/create-agent.dto';
 
 @Injectable()
 export class AgentService {
   constructor(
     @InjectRepository(Agent) private agentRepository: Repository<Agent>,
   ) {}
+
+  public async createAgent(createAgentDto: CreateAgentDto) {
+    const { name, email, phoneNumber, agencyId } = createAgentDto;
+
+    //!TODO: work will start from here next day
+  }
 }
