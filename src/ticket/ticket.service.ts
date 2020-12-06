cfcf4a2198ca63035e2e5bd96431375c6e14074c
mobin-zaman <mobinzamandev@gmail.com>
2020-12-06 01:06:10 -0500

    get tickets of an agent added

--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Ticket } from './ticket.entity';
+import {InjectRepository} from "@nestjs/typeorm";
+import { Agent } from 'src/auth/agent.entity';
 
 @Injectable()
-export class TicketService {}
+export class TicketService {
+    constructor(
+       @InjectRepository(Ticket) private ticketRepository: Repository<Ticket>,
+    ) {}
+
+    async getAllTicketsAgent(currentUser:Agent) {
+        return await currentUser.tickets;
+    }
+}
